c3419a14f45bd467892003a7b66e9e5b57bee746
nikhilrajbhar <nikhilrajbhar013@gmail.com>
2024-01-07 17:16:01 +0530

    main working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AboutComponent } from './project-component/about/about.component';
 import { MainComponent } from './project-component/main/main.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    AboutComponent,
     MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
